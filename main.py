ede9818d3df6dd59332e9b2f9e7107aa7876cbd6
TheBarzani <ismaelmergasori@gmail.com>
2024-01-20 12:34:36 -0500

    add simple experimental plotting view

--- main.py
+++ main.py
@@ -3,25 +3,38 @@
 # json file
  
 import json
+import matplotlib.pyplot as plt
 
-def main():
+def simple_graph():
     # Opening JSON file
     e1 = open('data/Exchange_1.json')
-    e2 = open('data/Exchange_2.json')
-    e3 = open('data/Exchange_3.json')
     # returns JSON object as 
     # a dictionary
     data = json.load(e1)
     
     # Iterating through the json
     # list
+
+    X = []
+    Y = []
+
     for i in data:
-        print(i["TimeStamp"])
-    
-    # Closing file
+        if i["MessageType"] == "NewOrderAcknowledged":
+            X.append(int(i["TimeStampEpoch"]))
+            Y.append(i["OrderPrice"])
+            print(i["OrderID"])
+
+    plt.ion() # turn interactive mode on
+    animated_plot = plt.plot(X, Y, 'ro')[0]
+
+    for i in range(len(X)):
+        animated_plot.set_xdata(X[0:i])
+        animated_plot.set_ydata(Y[0:i])
+        plt.draw()
+        plt.pause(0.1)
+        
+    #Closing file
     e1.close()
-    e2.close()
-    e3.close()
 
 if __name__ == "__main__":
-    main()
+    simple_graph()
